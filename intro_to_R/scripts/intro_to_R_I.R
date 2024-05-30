6c1fd0a66a05bb3316cf6b981b71789632a85a18
Abigail Shockey <abbashockey@gmail.com>
2024-05-30 13:10:31 -0500

    Create intro_to_R_I.R

---
+++ intro_to_R/scripts/intro_to_R_I.R
@@ -0,0 +1,48 @@
+# Introduction to R part I: Simple mathematics
+
+### R can be used to perform mathematical equations.
+### Addition is performed using + :
+
+2 + 2
+
+### Subtraction is performed using - :
+
+4 - 2
+
+### Multiplication can be performed using * :
+
+2 * 2
+
+### Exponents can be calculated using ** or ^:
+
+2**2
+2^2
+
+### R can also be used for mathematical comparisons.
+### Equal to:
+
+2 == 2
+  
+### Greater-than/less-than:
+
+4 > 3
+
+### Greater-than or equal to/less-than or equal to:
+
+4 >= 2
+
+2 <= 4
+
+### Not equal to:
+
+3 != 2
+
+### When a comparison is true, R returns TRUE, and when a comparison is false, 
+### R returns FALSE
+### This statement would return FALSE:
+
+2 > 3
+
+### And this statement would return TRUE:
+
+3 > 2
